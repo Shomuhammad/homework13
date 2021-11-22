0ae1281f9d34b815e17cecc2a7d73991a0213916
Asus <Shomuhammad>
2021-11-22 19:56:47 +0600

    homework13

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
 
+
     public static void main(String[] args) {
-	
+       Bank bank = new Bank();
+       bank.setPassvord("Alisher");
+       bank.setAmount(25);
+        System.out.println(bank.getPassvord());
     }
 }
